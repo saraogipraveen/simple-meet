68e943dc960a721cab09c2e489674deaa158a09e
Praveen Saraogi <saraogipraveen@gmail.com>
2020-05-02 00:11:39 +0530

    1. icons added 2. EventTypes comp

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,69 @@
 import React from 'react';
-import { Tabs } from 'antd';
+import { Tabs, Row, Col, Button, Card, Divider } from 'antd';
+import { PlusOutlined, EditOutlined } from '@ant-design/icons';
 
 const { TabPane } = Tabs;
+
+function EventTypes() {
+
+  const cardStyle = { borderTop: "2px solid blue", borderRadius: "4px" };
+
+
+  return (
+    <>
+      <Row>
+        <Col span={8}>
+          My Link
+      <br />
+          <a href="">appointment.com/sdsasa </a>
+        </Col>
+        <Col span={4} offset={12}>
+          <Button icon={<PlusOutlined />}>New Event Type</Button>
+        </Col>
+      </Row>
+      <Divider orientation="left" style={{ color: '#333', fontWeight: 'normal' }} />
+      <Row gutter={16}>
+        <Col span={8}>
+          <Card
+            style={{...cardStyle}}
+            actions={[
+              <a href="">Edit </a>
+            ]}>
+            Demo Call
+            <br/>
+            30 min
+        </Card>
+        </Col>
+        <Col span={8}>
+          <Card
+            style={{...cardStyle}}
+            actions={[
+              <a href="">Edit </a>
+            ]}>
+            Demo Call <br/>
+            30 min
+        </Card>
+        </Col>
+        <Col span={8} >
+          <Card
+            style={{...cardStyle}}
+            actions={[
+              <a href="">Edit </a>
+            ]} >
+            Demo Call<br/>
+            30 min
+        </Card>
+        </Col>
+      </Row>
+    </>
+  )
+}
+
+function ScheduledEvents() {
+  return (
+    'schedlued meeting'
+  )
+}
 
 const Home = () => {
   return (
@@ -9,11 +71,11 @@ const Home = () => {
       <h1>My Schedule</h1>
       <Tabs defaultActiveKey="1">
         <TabPane tab="Event Types" key="1">
-          Event Types
-    </TabPane>
+          <EventTypes />
+        </TabPane>
         <TabPane tab="Scheduled Events" key="2">
-           Events
-    </TabPane>
+          <ScheduledEvents />
+        </TabPane>
       </Tabs>
     </div>
   )
